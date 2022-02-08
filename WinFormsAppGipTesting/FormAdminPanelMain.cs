507bcd401dbe16e4f87026e2fa013e2952b8f872
Ilias-Op-De-Fiets <iliastaouil.sci@gmail.com>
2022-02-08 12:05:22 +0100

    part 1 new db

--- WinFormsAppGipTesting/FormAdminPanelMain.cs
+++ WinFormsAppGipTesting/FormAdminPanelMain.cs
@@ -6,15 +6,22 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace WinFormsAppGipTesting
 {
     public partial class FormAdminPanelMain : Form
     {
         public FormAdminPanelMain()
-        {
+        {   
             InitializeComponent();
         }
+
+        //initialising sql classes
+        MySqlConnection con = new MySqlConnection();
+        MySqlCommand cmd = new MySqlCommand();
+        MySqlDataAdapter da = new MySqlDataAdapter();
+        DataTable dt = new DataTable();
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -25,7 +32,40 @@ namespace WinFormsAppGipTesting
 
         private void FormAdminPanelMain_Load(object sender, EventArgs e)
         {
-            //dataGridViewAdminAccounts.DataSource = data
+            //Setting connection string
+            con.ConnectionString = "server=mysql.sintcordula.be;user id=troublefinder_usr;password=7a3Gf3VY;persistsecurityinfo=True;database=troublefinder";
+
+            string strQuery;
+            try
+            {
+                con.Open();
+                strQuery = "SELECT * FROM AdminAccounts";
+                
+                cmd = new MySqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = strQuery;
+
+                da = new MySqlDataAdapter();
+                da.SelectCommand = cmd;
+
+                dt = new DataTable();
+
+                da.Fill(dt);
+
+                dataGridViewAdminAccounts.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                //catching error
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                da.Dispose();
+                con.Close();
+            }
+
+            
         }
     }
 }
