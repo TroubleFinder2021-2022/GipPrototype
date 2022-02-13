eb94d2f40e17d8715e155ba3482993255727d068
Ilias-Op-De-Fiets <iliastaouil.sci@gmail.com>
2022-02-13 21:53:30 +0100

    crud class part 1

---
+++ WinFormsAppGipTesting/DbSolution.cs
@@ -0,0 +1,105 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAppGipTesting
+{
+    class DbSolution
+    {
+        public static MySqlConnection GetConnection()
+        {
+            string strSql = "server=84.198.150.18;user id=troublefinder_usr;password=7a3Gf3VY;persistsecurityinfo=True;database=troublefinder";
+            MySqlConnection con = new MySqlConnection(strSql);
+
+            try
+            {
+                con.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("MySQL Connection failed! \n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return con;
+        }
+
+        public static void AddSolution(Solution sol)
+        {
+            string strSql = "INSERT INTO solutions VALUES (NULL, @SolProblem, @SolSolution, @SolCategory, @SolSubCategory)";
+            MySqlConnection con = GetConnection();
+            MySqlCommand cmd = new MySqlCommand(strSql, con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@SolProblem", MySqlDbType.VarChar).Value = sol.problem;
+            cmd.Parameters.Add("@SolSolution", MySqlDbType.VarChar).Value = sol.solution;
+            cmd.Parameters.Add("@SolCategory", MySqlDbType.VarChar).Value = sol.category;
+            cmd.Parameters.Add("@SolSubCategory", MySqlDbType.VarChar).Value = sol.subcategory;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Solution added.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Solution could not be added. \n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+        public static void UpdateSolution(Solution sol, string id)
+        {
+            string strSql = "UPDATE solutions SET problem = @SolProblem, solution = @SolSolution, category = @SolCategory, subcategory = @SolSubCategory WHERE ID = @SolID";
+            MySqlConnection con = GetConnection();
+            MySqlCommand cmd = new MySqlCommand(strSql, con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@SolProblem", MySqlDbType.VarChar).Value = sol.problem;
+            cmd.Parameters.Add("@SolSolution", MySqlDbType.VarChar).Value = sol.solution;
+            cmd.Parameters.Add("@SolCategory", MySqlDbType.VarChar).Value = sol.category;
+            cmd.Parameters.Add("@SolSubCategory", MySqlDbType.VarChar).Value = sol.subcategory;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Updated Successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Solution could not be updated. \n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+        public static void DeleteSolution(string id)
+        {
+            string strSql = "DELETE FROM solutions WHERE ID = @SolID";
+            MySqlConnection con = GetConnection();
+            MySqlCommand cmd = new MySqlCommand(strSql, con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@SolID", MySqlDbType.VarChar).Value = id;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted Successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Solution could not be deleted. \n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+        public static void DisplayAndSearchSolution(string strQuery, DataGridView dgv)
+        {
+            string strSql = strQuery;
+            MySqlConnection con = GetConnection();
+            MySqlCommand cmd = new MySqlCommand(strSql, con);
+            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+            DataTable tbl = new DataTable();
+            adp.Fill(tbl);
+            con.Close();
+            //36:00
+        }
+    }
+}
---
+++ WinFormsAppGipTesting/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsAppGipTesting
+{
+    class Solution
+    {
+        public string problem { get; set; }
+        public string solution { get; set; }
+        public string category { get; set; }
+        public string subcategory { get; set; }
+
+        public Solution(string problem, string solution, string category, string subcategory)
+        {
+            this.problem = problem;
+            this.solution = solution;
+            this.category = category;
+            this.subcategory = subcategory;
+        }
+        
+    }
+}
